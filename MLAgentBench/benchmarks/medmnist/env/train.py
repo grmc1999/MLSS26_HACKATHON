a777ba8f28c684389d2f86f9506f3464a980343b
Emilio Ochoa Alva <54749384+Alighieri1231@users.noreply.github.com>
2026-06-22 11:24:58 -0400

    experiment: LeakyReLU(0.1) for better gradient flow

--- MLAgentBench/benchmarks/medmnist/env/train.py
+++ MLAgentBench/benchmarks/medmnist/env/train.py
@@ -33,11 +33,11 @@ class SimpleCNN(nn.Module):
         self.num_classes = num_classes
 
     def forward(self, x):
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.leaky_relu(self.bn1(self.conv1(x)), 0.1))
+        x = self.pool(F.leaky_relu(self.bn2(self.conv2(x)), 0.1))
         x = x.view(x.size(0), -1)
         x = self.dropout(x)
-        x = F.relu(self.fc1(x))
+        x = F.leaky_relu(self.fc1(x), 0.1)
         x = self.fc2(x)
         return x
 
