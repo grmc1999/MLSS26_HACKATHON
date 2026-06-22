fe38589d631c95b19b5734987dd1907c0254a2b6
Emilio Ochoa Alva <54749384+Alighieri1231@users.noreply.github.com>
2026-06-22 11:34:21 -0400

    Revert "experiment: add third conv block (128 channels) for deeper features"
    This reverts commit 38e9cbef6708b720e6f9e957949e195a86fbc5c9.

--- MLAgentBench/benchmarks/medmnist/env/train.py
+++ MLAgentBench/benchmarks/medmnist/env/train.py
@@ -26,19 +26,16 @@ class SimpleCNN(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
         self.pool = nn.MaxPool2d(2)
         self.dropout = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.25)
-        self.fc1 = nn.Linear(128 * 7 * 7, 128)
+        self.fc1 = nn.Linear(64 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 3)
         self.num_classes = num_classes
 
     def forward(self, x):
-        x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1)
+        x = self.pool(F.leaky_relu(self.bn1(self.conv1(x)), 0.1))
         x = self.pool(F.leaky_relu(self.bn2(self.conv2(x)), 0.1))
-        x = self.pool(F.leaky_relu(self.bn3(self.conv3(x)), 0.1))
         x = x.view(x.size(0), -1)
         x = self.dropout(x)
         x = F.leaky_relu(self.fc1(x), 0.1)
