e773e73f9c32d67af8eb1612609b25de19eda18b
Emilio Ochoa Alva <54749384+Alighieri1231@users.noreply.github.com>
2026-06-22 11:33:26 -0400

    Revert "experiment: temperature scaling T=2.0 for calibrated softmax"
    This reverts commit a3157feead67b2eb7f88fe15c327eb2fb7be57dd.

--- MLAgentBench/benchmarks/medmnist/env/train.py
+++ MLAgentBench/benchmarks/medmnist/env/train.py
@@ -31,7 +31,6 @@ class SimpleCNN(nn.Module):
         self.dropout2 = nn.Dropout(0.25)
         self.fc1 = nn.Linear(64 * 7 * 7, 128)
         self.fc2 = nn.Linear(128, 3)
-        self.temperature = 2.0
         self.num_classes = num_classes
 
     def forward(self, x):
@@ -41,7 +40,7 @@ class SimpleCNN(nn.Module):
         x = self.dropout(x)
         x = F.leaky_relu(self.fc1(x), 0.1)
         x = self.dropout2(x)
-        x = self.fc2(x) / self.temperature
+        x = self.fc2(x)
         return x
 
 
